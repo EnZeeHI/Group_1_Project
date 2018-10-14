dfb436a342ff1a4eb19e44820f314c98736a1d18
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2018-10-14 20:15:29 +0200

    Added a trigger
    the trigger flipsonly flips the switch for now

---
+++ TEST/Assets/LeverTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrigger : MonoBehaviour {
+ public bool colliding;
+
+
+		void OnTriggerEnter2D(Collider2D other){
+				
+
+    		//Debug.Log(other.name);
+			//Debug.Log("Object entered a trigger");
+
+		}
+		void OnTriggerStay2D (Collider2D other){
+			//Debug.Log("object is within the trigger");
+			// var gameObject = GameObject;
+			 colliding = true;
+			
+		 var SpriteRenderer  = gameObject.GetComponent<SpriteRenderer>();
+			 if (Input.GetKeyDown(KeyCode.F)&& colliding==true){
+					 if(SpriteRenderer.flipX== false){
+					SpriteRenderer.flipX = true;
+					}
+					else {
+						SpriteRenderer.flipX = false;
+						}
+				}
+	
+			
+
+				
+		}
+		void OnTriggerExit2D(Collider2D other){
+			//Debug.Log("object is within the trigger");
+			colliding = false;
+		}
+		
+	 
+	// void update(){
+	// 	 var SpriteRenderer  = gameObject.GetComponent<SpriteRenderer>();
+	// 		 if (Input.GetKeyDown(KeyCode.F)&& colliding==true)
+	// 			{ 
+	// 				SpriteRenderer.flipX = true;
+	// 			}
+	// }
+
+	
+}
