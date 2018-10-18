a3dc0c3f9d2a716895a9fbe8afa06a0c1e0bd1b7
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2018-10-18 14:45:02 +0200

    marcel, fix this

---
+++ TEST/TEST/Assets/LeverTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrigger : MonoBehaviour {
+public bool Colliding;
+public bool TriggerActive;
+	// void update(){
+	// 	OnTriggerStay2D(Collider2D other);
+	// }
+	
+	// function OnTriggerStay - is called when two colliders overlap
+	// allows you to use the trigger if anything is touching it
+		void OnTriggerStay2D (Collider2D other){
+			
+			 Colliding = true;
+			
+		 var SpriteRenderer  = gameObject.GetComponent<SpriteRenderer>();
+		 // checks enables and dissables the trigger, flips the sprite
+			 if (Input.GetKeyDown(KeyCode.F)&& Colliding==true){
+				
+					 if(SpriteRenderer.flipX== false){
+					SpriteRenderer.flipX = true;
+					TriggerActive = true;
+					
+					}
+					else {
+						SpriteRenderer.flipX = false;
+						TriggerActive = false;
+						}
+						 Debug.Log(SpriteRenderer.flipX);
+				}
+	
+			
+
+				
+		}
+		//function OnTriggerExit2D is called  when the coliders no longer overlap
+		// makes that you cant use the trigger when you dont touch it
+		void OnTriggerExit2D(Collider2D other){
+			
+			Colliding = false;
+		}
+	
+	 
+
+
+	
+}
